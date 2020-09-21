22a88b790c68075c7e49e5f5227503a919b5df6b
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-09-21 11:40:06 +0200

    ApplicationDbContext.cs added

---
+++ MedicalAppointment.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalAppointment.Infrastructure.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+    }
+}
