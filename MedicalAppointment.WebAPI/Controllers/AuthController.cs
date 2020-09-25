3db7ea623d47b3ae023998832d9c398718f08c9c
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-09-25 08:50:02 +0200

    AuthController.cs added

---
+++ MedicalAppointment.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using MedicalAppointment.Core.Interfaces;
+using MedicalAppointment.Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MedicalAppointment.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
+        private readonly IJwtTokenService _jwtTokenService;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthController(
+            IConfiguration config,
+            IMapper mapper,
+            IJwtTokenService jwtTokenService,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager)
+        {
+            _config = config;
+            _mapper = mapper;
+            _jwtTokenService = jwtTokenService;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
