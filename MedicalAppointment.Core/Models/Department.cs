9c2d9da846edd5e9e912f7aa258c0b02e7c267da
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-09-27 17:29:18 +0200

    Doctor and Department models added

---
+++ MedicalAppointment.Core/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalAppointment.Core.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Doctor> Doctors { get; set; }
+    }
+}
---
+++ MedicalAppointment.Core/Models/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalAppointment.Core.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Education { get; set; }
+        public string Designation { get; set; }
+        public string Gender { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
