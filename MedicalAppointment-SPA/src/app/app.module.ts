345cd47e32cdcdd8bd3f04166fd70b230544e425
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-10-12 17:08:17 +0200

    JwtToken added

--- MedicalAppointment-SPA/src/app/app.module.ts
+++ MedicalAppointment-SPA/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JwtModule } from '@auth0/angular-jwt/lib/angular-jwt.module';
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['localhost:5000'],
+        blacklistedRoutes: ['localhost:5000/api/auth']
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
