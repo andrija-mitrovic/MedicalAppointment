7c0f154fbf9ad316553831d5109896d50df1ac78
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-11-22 10:10:43 +0100

    Register component UI reworked

--- MedicalAppointment-SPA/src/app/_models/user.ts
+++ MedicalAppointment-SPA/src/app/_models/user.ts
@@ -2,5 +2,6 @@ export interface User {
     id: number;
     userName: string;
     gender: string;
+    password: string;
     dateOfBirth: Date;
 }
--- MedicalAppointment-SPA/src/app/pages/register/register.component.ts
+++ MedicalAppointment-SPA/src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(private auth: AuthService,
+              private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit() {
+    this.createRegisterForm();
   }
 
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 }
