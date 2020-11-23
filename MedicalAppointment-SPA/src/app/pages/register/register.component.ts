5ed571b0ddd8108c6eb264c32ba1ee2124ed946c
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-11-23 08:58:50 +0100

    Register functionality added

--- MedicalAppointment-SPA/src/app/pages/register/register.component.ts
+++ MedicalAppointment-SPA/src/app/pages/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from 'src/app/_models/user';
 import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
@@ -10,6 +11,8 @@ import { AuthService } from 'src/app/_services/auth.service';
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
+  user: User;
+
   constructor(private auth: AuthService,
               private fb: FormBuilder,
               private router: Router) { }
@@ -26,4 +29,20 @@ export class RegisterComponent implements OnInit {
       password: ['', Validators.required]
     });
   }
+
+  register() {
+    if(this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.auth.register(this.user).subscribe(() => {
+        console.log('Registration successful');
+      }, error => {
+        console.log(error);
+      },() => {
+        this.auth.login(this.user).subscribe(() => {
+          this.router.navigate(['/employees']);
+        });
+      });
+    }
+  }
+
 }
